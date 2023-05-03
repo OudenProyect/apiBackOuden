5550d4dcdbcbb3212e4be53c87b6a0643d54ccd6
strom99 <alissoncc17@gmail.com>
2023-05-03 17:32:57 +0200

    buscador de collection casas

--- src/Controller/SearchController.php
+++ src/Controller/SearchController.php
@@ -27,25 +27,35 @@ class SearchController extends AbstractController
         try {
             $locats = $services->findBy(['name' => $search->get('ubicacion')]);
 
-            if (count($locats) > 0) {
-                foreach ($publication->findAll() as $publi) {
-                    foreach ($publi->getHouse()->getAreaServices() as $service) {
-                        if (strtolower($service->getName()) == strtolower($search->get('ubicacion'))) {
-                            // devolvemos la publicacion con todos sus datos de la casas etc
-                            $result[] = $publi;
-                        }
-                    }
+            if (count($locats[0]->getHouses()) > 0) {
+                foreach ($locats[0]->getHouses() as $h) {
+                    // dd($publication->findBy(['house' => $h->getId()]));
+                    $publi = $publication->findBy(['house' => $h->getId()]);
+                    // dd($publi);
+                    $result[] = $publi;
                 }
             }
+
+            // if (count($locats[0]) > 0) {
+            //     foreach ($publication->findAll() as $publi) {
+            //         foreach ($publi->getHouse()->getAreaServices() as $service) {
+            //             if (strtolower($service->getName()) == strtolower($search->get('ubicacion'))) {
+            //                 // devolvemos la publicacion con todos sus datos de la casas etc
+            //                 $result[] = $publi;
+            //             }
+            //         }
+            //     }
+            // }
         } catch (Exception $e) {
         }
         return $this->json(
             $result,
             200,
             [],
-            [AbstractNormalizer::CIRCULAR_REFERENCE_HANDLER => function () {
-                return 'self';
-            }]
+            // [AbstractNormalizer::CIRCULAR_REFERENCE_HANDLER => function () {
+            //     return 'self';
+            // }]
+            [AbstractNormalizer::IGNORED_ATTRIBUTES => ['publications', 'publication', 'houses']]
         );
     }
 
