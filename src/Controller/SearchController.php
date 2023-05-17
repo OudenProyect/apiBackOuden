b99bb5316eb5d1ff4773c34a37d8c19f78dcfbfb
strom99 <alissoncc17@gmail.com>
2023-05-17 16:33:54 +0200

    filtros terminados

--- src/Controller/SearchController.php
+++ src/Controller/SearchController.php
@@ -8,6 +8,8 @@ use App\Repository\LocationRepository;
 use App\Repository\LocationServicesRepository;
 use App\Repository\PublicationRepository;
 use App\Repository\UserRepository;
+use Doctrine\ORM\EntityManagerInterface;
+use Doctrine\ORM\Mapping\OrderBy;
 use Exception;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
@@ -25,7 +27,7 @@ class SearchController extends AbstractController
         $result = [];
 
         try {
-            if($search->get('ubicacion') != 'all'){
+            if ($search->get('ubicacion') != 'all') {
                 $locats = $services->findBy(['name' => $search->get('ubicacion')]);
 
                 // buscamos en los servicios las casas relacionadas con la ubicacion y buscamos la publicacion
@@ -34,14 +36,12 @@ class SearchController extends AbstractController
                         $publi = $publication->findBy(['house' => $h->getId()]);
                         $result = array_merge($result, $publi);
                     }
-                }else{
+                } else {
                     $result = 'No hay casas en esta ubicacion';
                 }
-            }else{
+            } else {
                 $result = $publication->findAll();
             }
-            
-
         } catch (Exception $e) {
         }
         return $this->json(
@@ -60,7 +60,83 @@ class SearchController extends AbstractController
     {
         // recibimos lo que busca el usuario @param string
         $casa = $casas->findBy(['location' => 2]);
-        dd($casa);
         return $this->json($casa);
+    }
+
+    #[Route('/filtrar', name: 'app_filtrar')]
+    public function filtros(Request $request, EntityManagerInterface $entityManager, PublicationRepository $publi)
+    {
+        $res = [];
+        $tipo = $request->get('tipo');
+        $hab = $request->get('hab');
+        $bath = $request->get('bath');
+        $pricemin = $request->get('pricemin');
+        $pricemax = $request->get('pricemax');
+        $surfacemin = $request->get('surfacemin');
+        $surfacemax = $request->get('surfacemax');
+        $extras = $request->get('extras') ? explode(',', $request->get('extras')) : [];
+
+        $queryBuilder = $entityManager->createQueryBuilder();
+
+        $queryBuilder->select('p')
+            ->from('App\Entity\House', 'p');
+
+        if (count($extras) > 0) {
+            $queryBuilder->leftJoin('p.feature', 'he');
+        }
+        if ($tipo != "Indiferent") {
+            $queryBuilder->where('p.type = :tipo')
+                ->setParameter('tipo', $tipo);
+        }
+
+        if ($hab != 'null') {
+            $queryBuilder->andWhere('p.n_bedrooms = :hab')
+                ->setParameter('hab', $hab);
+        }
+        if ($bath != 'null') {
+            $queryBuilder->andWhere('p.toilets = :bath')
+                ->setParameter('bath', $bath);
+        }
+        if ($pricemin != 'Indiferent' && $pricemax != 'Indiferent') {
+            $queryBuilder->andWhere('p.price BETWEEN :pricemin AND :pricemax')
+                ->setParameter('pricemin', $pricemin)
+                ->setParameter('pricemax', $pricemax);
+        }
+
+        if ($surfacemin != 'Indiferent' && $surfacemax != 'Indiferent') {
+            $queryBuilder->andWhere('p.UsefulLivinArea BETWEEN :surfacemin AND :surfacemax')
+                ->setParameter('surfacemin', (int) $surfacemin)
+                ->setParameter('surfacemax', (int) $surfacemax);
+        }
+
+        if (count($extras) > 0) {
+            $queryBuilder->andWhere(
+                $queryBuilder->expr()->in('he.id', $extras)
+            )
+                ->groupBy('p.id')
+                ->having($queryBuilder->expr()->eq($queryBuilder->expr()->countDistinct('he.id'), 2));
+        }
+        $query = $queryBuilder->getQuery()->getResult();
+
+        if (count($query) > 0) {
+            foreach ($query as $casa) {
+                $casaid = $casa->getId();
+                $p = $publi->findBy(['house' => $casaid]);
+                if (count($p) > 0) {
+                    $res = array_merge($res, $p);
+                }
+                //     $publi = $publication->findBy(['house' => $h->getId()]);
+                //     $result = array_merge($result, $publi);
+            }
+        }
+
+
+        // returns an array of Product objects
+        return $this->json(
+            $res,
+            200,
+            [],
+            [AbstractNormalizer::IGNORED_ATTRIBUTES => ['publications', 'publication', 'houses']]
+        );
     }
 }
--- src/Repository/PublicationRepository.php
+++ src/Repository/PublicationRepository.php
@@ -39,28 +39,37 @@ class PublicationRepository extends ServiceEntityRepository
         }
     }
 
-//    /**
-//     * @return Publication[] Returns an array of Publication objects
-//     */
-//    public function findByExampleField($value): array
-//    {
-//        return $this->createQueryBuilder('p')
-//            ->andWhere('p.exampleField = :val')
-//            ->setParameter('val', $value)
-//            ->orderBy('p.id', 'ASC')
-//            ->setMaxResults(10)
-//            ->getQuery()
-//            ->getResult()
-//        ;
-//    }
 
-//    public function findOneBySomeField($value): ?Publication
-//    {
-//        return $this->createQueryBuilder('p')
-//            ->andWhere('p.exampleField = :val')
-//            ->setParameter('val', $value)
-//            ->getQuery()
-//            ->getOneOrNullResult()
-//        ;
-//    }
+
+    // public function findAllOrderedByNombre(Publication $entity)
+    // {
+    //     return $this->createQueryBuilder('a')
+    //         ->addOrderBy('a.nombre', 'ASC')
+    //         ->getResult();
+    // }
+
+    //    /**
+    //     * @return Publication[] Returns an array of Publication objects
+    //     */
+    //    public function findByExampleField($value): array
+    //    {
+    //        return $this->createQueryBuilder('p')
+    //            ->andWhere('p.exampleField = :val')
+    //            ->setParameter('val', $value)
+    //            ->orderBy('p.id', 'ASC')
+    //            ->setMaxResults(10)
+    //            ->getQuery()
+    //            ->getResult()
+    //        ;
+    //    }
+
+    //    public function findOneBySomeField($value): ?Publication
+    //    {
+    //        return $this->createQueryBuilder('p')
+    //            ->andWhere('p.exampleField = :val')
+    //            ->setParameter('val', $value)
+    //            ->getQuery()
+    //            ->getOneOrNullResult()
+    //        ;
+    //    }
 }
